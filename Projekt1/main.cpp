d7cbed18988ca29331b148dafdce7c6f139f6a12
Bartosz Ruszczak <bruszczak48@gmail.com>
2017-10-22 12:47:21 +0200

    created a static body

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -7,13 +7,24 @@
 int main(int argc, char** argv)
 {
 	//gravity vector;
-	b2Vec2 myGravity(10.0f, -20.0 );
+	b2Vec2 myGravity(10.0f, -20.0f );
 	//adding gravity to my world;
 	b2World myWorld(myGravity);
+
+	//creating a body
+
 	// ground body definition
 	b2BodyDef myGroundBodyDef;
 	// setting initial position for body
 	myGroundBodyDef.position.Set(10.f, -15.0f);
+	// passed definition to the world
+	b2Body* myGroundBody = myWorld.CreateBody(&myGroundBodyDef);
+	//creating a ground polygon
+	b2PolygonShape myGroundBox;
+	//setting width (2x30) and height (2x20)
+	myGroundBox.SetAsBox(30.0f, 20.0f);
+	//creating fixture with a shortcut 'cause we do not need any specific properties (density, mass, ect.)  to this body
+	myGroundBody->CreateFixture(&myGroundBox, 0.0f);
 
 	return 0;
 }
