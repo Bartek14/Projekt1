bd5475538b30c5ac45e3636d887bbdfc4d4cd30e
Bartosz Ruszczak <bruszczak48@gmail.com>
2017-12-01 15:43:23 +0100

    coordinates packed in structures

--- Projekt1/main.cpp
+++ Projekt1/main.cpp
@@ -6,43 +6,68 @@
 
 int main(int argc, char** argv)
 {
+	struct coOrdinates
+	{
+		float32 x = 0;
+		float32 y = 0;
+		float32 width = 0;
+		float32 height = 0;
+
+	};
 	/////////////Graphics
 
+	coOrdinates dynamicRectCoordinates =
+	{
+		0,
+		0,
+		100,
+		100,
+	};
+	coOrdinates groundRectCoordinates =
+	{
+		0,
+		300,
+		100,
+		100,
+	};
 	float32 dynamicRectPositionX = 0;
 	float32 dynamicRectPositionY = 0;
 	float32 groundRectPositionX = 0;
 	float32 groundRectPositionY = 300;
 	sf::RenderWindow window(sf::VideoMode(400, 400), "SFML works!");
-	sf::RectangleShape dynamicRect(sf::Vector2f(100, 100));
+	sf::RectangleShape dynamicRect(sf::Vector2f(dynamicRectCoordinates.width, dynamicRectCoordinates.height));
 	dynamicRect.setFillColor(sf::Color::Green);
-	dynamicRect.setPosition(dynamicRectPositionX, dynamicRectPositionY);
+	dynamicRect.setPosition(dynamicRectCoordinates.x, dynamicRectCoordinates.y);
 
-	sf::RectangleShape groundRect(sf::Vector2f(100, 100));
+	sf::RectangleShape groundRect(sf::Vector2f(groundRectCoordinates.width, groundRectCoordinates.height));
 	groundRect.setFillColor(sf::Color::Red);
-	groundRect.setPosition(groundRectPositionX, groundRectPositionY);
-	int iterator(0);
-
+	groundRect.setPosition(groundRectCoordinates.x, groundRectCoordinates.y);
+	//int iterator(0);
+	//int temp;
 	
-
+	//b2Vec2 myForce(1.0f, 0.0f);
 
 
 	//gravity vector;
-	b2Vec2 myGravity(0.0f, 10.0f );
+	b2Vec2 myGravity(0.0f, 0.5f);
 	//adding gravity to my world;
 	b2World myWorld(myGravity);
+
+	//changing angle
+	//b2Vec2 myAngle(50.0f, 50.0f);
 
 	//creating a static body (they are static by default)
 
 	// ground body definition
 	b2BodyDef myGroundBodyDef;
 	// setting initial position for body
-	myGroundBodyDef.position.Set(groundRectPositionX, groundRectPositionY);
+	myGroundBodyDef.position.Set(groundRectCoordinates.x, groundRectCoordinates.y);
 	// passed definition to the world
 	b2Body* myGroundBody = myWorld.CreateBody(&myGroundBodyDef);
 	//creating a ground polygon
 	b2PolygonShape myGroundBox;
-	//setting width (2x30) and height (2x5)
-	myGroundBox.SetAsBox(50.0f, 50.0f);
+	//setting width (2x50) and height (2x50)
+	myGroundBox.SetAsBox(groundRectCoordinates.width/2, groundRectCoordinates.height/2);
 	//creating fixture with a shortcut 'cause we do not need any specific properties (density, mass, ect.)  to this body
 	myGroundBody->CreateFixture(&myGroundBox, 0.0f);
 
@@ -56,12 +81,12 @@ int main(int argc, char** argv)
 
 	//creating the box shape
 	b2PolygonShape myDynamicBox;
-	myDynamicBox.SetAsBox(50.0f, 50.0f);
+	myDynamicBox.SetAsBox(dynamicRectCoordinates.width/2, dynamicRectCoordinates.height/2);
 	//creating fixture definition using the box
 	b2FixtureDef myFixtureDef;
 	myFixtureDef.shape = &myDynamicBox;
 	myFixtureDef.density = 1.0f;
-	myFixtureDef.friction = 0.9f;
+	myFixtureDef.friction = 90.0f;
 
 	//creating fixture
 	myDynamicBody->CreateFixture(&myFixtureDef);
@@ -85,7 +110,7 @@ int main(int argc, char** argv)
 		}
 		//game loop
 		//for (int32 i = 0; i < 59; ++i)
-		while (iterator<400)
+		while (sf::Keyboard::isKeyPressed)
 		{
 		
 			myWorld.Step(timeStep, myVelocityIterations, myPositonIterations);
@@ -97,7 +122,7 @@ int main(int argc, char** argv)
 			//Graphics
 
 			dynamicRect.setPosition(myPosition.x, myPosition.y);
-			iterator++;
+			//iterator++;
 			//displaying
 			window.clear();
 			window.draw(dynamicRect);
@@ -114,8 +139,6 @@ int main(int argc, char** argv)
 	//shapes
 
 	//circle
-
-
 //	b2BodyDef circleBodyDef;
 //	circleBodyDef.position.Set(0.0f, -9.0f);
 //	circleBodyDef.type = b2_dynamicBody;
